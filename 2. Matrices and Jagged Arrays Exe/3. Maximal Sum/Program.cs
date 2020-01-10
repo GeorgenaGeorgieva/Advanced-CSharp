0659768ae018168f07219e5e5822bd4835614bcd
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-10 12:16:58 +0200

    CSharp

---
+++ 2. Matrices and Jagged Arrays Exe/3. Maximal Sum/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Maximal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colsInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colsInfo[col];
+                }
+            }
+
+            int maxSum = int.MinValue;
+            int[,] tripleMatrix = new int[3, 3];
+
+            for (int row = 0; row < (matrix.GetLength(0) - 2); row++)
+            {
+                for (int col = 0; col < (matrix.GetLength(1) - 2); col++)
+                {
+                    int currentSum = matrix[row, col] + matrix[row, col + 1]
+                        + matrix[row, col + 2] + matrix[row + 1, col]
+                        + matrix[row + 1, col + 1] + matrix[row + 1, col + 2]
+                        + matrix[row + 2, col] + matrix[row + 2, col + 1]
+                        + matrix[row + 2, col + 2];
+
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+
+                        tripleMatrix = new int[,] {
+                            {matrix[row, col], matrix[row, col + 1], matrix[row, col + 2]},
+                            {matrix[row + 1, col], matrix[row + 1, col + 1], matrix[row + 1, col + 2]},
+                            {matrix[row + 2, col], matrix[row + 2, col + 1], matrix[row + 2, col + 2]}
+                        };
+                    }
+                }
+            }
+            Console.WriteLine($"Sum = {maxSum}");
+
+            for (int row = 0; row < tripleMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < tripleMatrix.GetLength(1); col++)
+                {
+                    Console.Write(tripleMatrix[row,col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 2. Matrices and Jagged Arrays Exe/4. Matrix shuffling/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _4._Matrix_shuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var colsInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colsInfo[col];
+                }
+            }
+
+            while (true)
+            {
+                string[] operations = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string command = operations[0];
+
+                if (command == "END")
+                {
+                    break;
+                }
+                else if (command == "swap" && operations.Length == 5)
+                {
+
+                    int firstRow = int.Parse(operations[1]);
+                    int firstCol = int.Parse(operations[2]);
+                    int secondRow = int.Parse(operations[3]);
+                    int secondCol = int.Parse(operations[4]);
+
+                    if (firstRow >= 0 && firstRow <= rows - 1 && firstCol >= 0 && firstCol <= cols - 1
+                        && secondRow >= 0 && secondRow <= rows - 1 && secondCol >= 0 && secondCol <= cols - 1)
+                    {
+                        var currentCell = matrix[firstRow, firstCol];
+                        matrix[firstRow, firstCol] = matrix[secondRow, secondCol];
+                        matrix[secondRow, secondCol] = currentCell;
+
+                        for (int row = 0; row < matrix.GetLength(0); row++)
+                        {
+                            for (int col = 0; col < matrix.GetLength(1); col++)
+                            {
+                                Console.Write(matrix[row, col] + " ");
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid input!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid input!");
+                }
+            }
+        }
+    }
+}
