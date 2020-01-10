3cad2fc3853115c8006c24896bb9912ed471545d
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-10 12:12:04 +0200

    CSharp

---
+++ 2. Matrices and Jagged Arrays Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _3._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colElements = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+            int sumDiagonalElements = 0;
+            int currentRow = 0;
+            int currentCol = 0;
+
+            while (true)
+            {
+                if (currentRow >= matrix.GetLength(0) || currentCol >= matrix.GetLength(1))
+                {
+                    break;
+                }
+                sumDiagonalElements += matrix[currentRow, currentCol];
+                currentRow++;
+                currentCol++;
+            }
+            Console.WriteLine(sumDiagonalElements);
+        }
+    }
+}
