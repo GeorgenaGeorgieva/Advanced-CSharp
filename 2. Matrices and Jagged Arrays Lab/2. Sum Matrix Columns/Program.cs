0aac9d3398cd38c5a6ee0830e759d9c3aea853d6
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-10 12:11:15 +0200

    CSharp

---
+++ 2. Matrices and Jagged Arrays Lab/2. Sum Matrix Columns/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Sum_Matrix_Columns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colInformation = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colInformation[col];
+                }
+            }
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int sumColsElements = 0;
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sumColsElements += matrix[row, col];
+                }
+                Console.WriteLine(sumColsElements);
+            }
+        }
+    }
+}
