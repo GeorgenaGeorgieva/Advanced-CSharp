ae89d0557bd5ea497ee591885e535c19fa1e7da5
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-10 12:14:26 +0200

    CSharp

---
+++ 2. Matrices and Jagged Arrays Lab/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+ 
+namespace _6._Jagged_Array_Modification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[][] jaggedArr = new int[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var currentRow = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                jaggedArr[row] = new int[currentRow.Length];
+
+                for (int col = 0; col < currentRow.Length; col++)
+                {
+                    jaggedArr[row][col] = currentRow[col];
+                }
+            }
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+
+                if (command.ToLower() == "end")
+                {
+                    break;
+                }
+                var commandParts = command.Split(" ");
+                var commandOperator = commandParts[0];
+                var commandRow = int.Parse(commandParts[1]);
+                var commandCol = int.Parse(commandParts[2]);
+                var commandValue = int.Parse(commandParts[3]);
+
+                if (commandRow < 0 || commandRow >= jaggedArr.Length 
+                    || commandCol < 0 || commandCol >= jaggedArr[commandRow].Length)
+                {
+                    Console.WriteLine($"Invalid coordinates");
+                    continue;
+                }
+                if (commandOperator.ToLower() == "add") 
+                {
+                    jaggedArr[commandRow][commandCol] += commandValue;
+                }
+                else if (commandOperator.ToLower() == "subtract")
+                {
+                    jaggedArr[commandRow][commandCol] -= commandValue;
+                }
+            }
+
+            foreach (var row in jaggedArr)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}
---
+++ 2. Matrices and Jagged Arrays Lab/7. Pascal Triangle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _7._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            if (rows >= 1 && rows <= 60)
+            {
+                int[][] pascalArr = new int[rows][];
+                pascalArr[0] = new int[1];
+                pascalArr[0][0] = 1; 
+
+                for (int row = 1; row < rows; row++)
+                {
+                    pascalArr[row] = new int[row + 1];
+                    pascalArr[row][0] = 1;
+                    pascalArr[row][pascalArr[row].Length - 1] = 1;
+
+                    for (int col = 1; col < pascalArr[row].Length - 1; col++)
+                    {
+                        pascalArr[row][col] += pascalArr[row - 1][col]
+                            + pascalArr[row - 1][col - 1];
+                    }
+                }
+
+                foreach (var row in pascalArr)
+                {
+                    Console.WriteLine(string.Join(" ", row));
+                }
+            }
+        }
+    }
+}
