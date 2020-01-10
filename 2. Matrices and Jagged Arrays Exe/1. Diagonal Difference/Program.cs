b3e17d6c916f9e31dcd586f05b4c5aeae3901d80
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-10 12:15:52 +0200

    CSharp

---
+++ 2. Matrices and Jagged Arrays Exe/1. Diagonal Difference/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace _1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeMatrix = int.Parse(Console.ReadLine());
+            int rows = sizeMatrix;
+            int cols = sizeMatrix;
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colsInforamtion = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colsInforamtion[col];
+                }
+            }
+            int sumPrimaryDiagonal = 0;
+            int currentRow = 0;
+            int currentCol = 0;
+
+            while (true)
+            {
+                if (currentRow >= matrix.GetLength(0) || currentCol >= matrix.GetLength(1))
+                {
+                    break;
+                }
+
+                sumPrimaryDiagonal += matrix[currentRow, currentCol];
+                currentRow++;
+                currentCol++;
+            }
+
+            int sumSecondaryDiagonal = 0;
+            currentRow = 0;
+            currentCol = matrix.GetLength(1) - 1;
+
+            while (true)
+            {
+                if (currentRow >= matrix.GetLength(0) || currentCol < 0)
+                {
+                    break;
+                }
+
+                sumSecondaryDiagonal += matrix[currentRow, currentCol];
+                currentRow++;
+                currentCol--;
+            }
+            int absDifference = Math.Abs(sumPrimaryDiagonal - sumSecondaryDiagonal);
+            Console.WriteLine(absDifference);
+        }
+    }
+}
---
+++ 2. Matrices and Jagged Arrays Exe/2. 2x2 Squares in Matrix/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _2._2x2_Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+            char[,] matrix = new char[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var colsInformation = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colsInformation[col];
+                }
+            }
+            int countOfEqualSquares = 0;
+            
+            for (int row = 0; row < (matrix.GetLength(0) - 1); row++)
+            {
+                for (int col = 0; col < (matrix.GetLength(1) - 1); col++)
+                {
+                    if (matrix[row,col] == matrix[row, col + 1] 
+                        && matrix[row, col] == matrix[row + 1, col] 
+                        && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        countOfEqualSquares++;
+                    }
+                }
+            }
+            Console.WriteLine(countOfEqualSquares);
+        }
+    }
+}
