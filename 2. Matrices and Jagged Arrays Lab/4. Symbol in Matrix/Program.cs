77e79f640bf7fac9d2203e03cc951e895f63c331
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-10 12:13:36 +0200

    CSharp

---
+++ 2. Matrices and Jagged Arrays Lab/4. Symbol in Matrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Symbol_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var colElements = Console.ReadLine();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+            char symbol = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }  
+            }
+            Console.WriteLine($"{symbol} does not occur in the matrix");
+        }
+    }
+}
---
+++ 2. Matrices and Jagged Arrays Lab/5. Square with Maximum Sum/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _5._Square_with_Maximum_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colElements = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+            int maxSum = int.MinValue;
+            int maxRolIndex = 0;
+            int maxColIndex = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    var currentSum = matrix[row, col] 
+                        + matrix[row, col + 1] 
+                        + matrix[row + 1, col] 
+                        + matrix[row + 1, col + 1];
+
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                        maxRolIndex = row;
+                        maxColIndex = col;
+                    }
+                }
+            }
+            Console.WriteLine($"{matrix[maxRolIndex, maxColIndex]} " +
+                $"{matrix[maxRolIndex, maxColIndex + 1]}");
+
+            Console.WriteLine($"{matrix[maxRolIndex + 1, maxColIndex]} " +
+                $"{matrix[maxRolIndex + 1, maxColIndex + 1]}");
+
+            Console.WriteLine(maxSum);
+        }
+    }
+}
