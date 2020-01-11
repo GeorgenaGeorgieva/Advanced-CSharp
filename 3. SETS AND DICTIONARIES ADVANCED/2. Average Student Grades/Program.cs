8c00c668c4edc92a006ce5d8a78fe041599810d3
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-11 22:43:44 +0200

    CSharp

---
+++ 3. SETS AND DICTIONARIES ADVANCED/2. Average Student Grades/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfStudents = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> studentsGrade = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < countOfStudents; i++)
+            {
+                string[] studentsInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = studentsInfo[0];
+                double grade = double.Parse(studentsInfo[1]);
+
+                if (!studentsGrade.ContainsKey(name))
+                {
+                    studentsGrade[name] = new List<double>();
+                }
+                studentsGrade[name].Add(grade);
+            }
+
+            foreach (var kvp in studentsGrade)
+            {
+                Console.Write($"{kvp.Key} -> ");
+
+                foreach (var grade in kvp.Value)
+                {
+                    Console.Write($"{grade:F2} ");
+                }
+                Console.WriteLine($"(avg: {kvp.Value.Average():F2})");
+            }
+        }
+    }
+}
