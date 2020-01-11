7e59e664fc86aa502acf339fc97bfeef26663ade
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-11 22:47:18 +0200

    CSharp

---
+++ 3. SETS AND DICTIONARIES ADVANCED/7. SoftUni Party/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace _7._SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vip = new HashSet<string>();
+            var regularGuests = new HashSet<string>();
+
+            while (true)
+            {
+                var inputline = Console.ReadLine();
+
+                if (inputline == "END")
+                {
+                    break;
+                }
+                else if (inputline == "PARTY")
+                {
+                    while (true)
+                    {
+                        var currentGuests = Console.ReadLine();
+
+                        if (IsMatched(currentGuests))
+                        {
+                            vip.Remove(currentGuests);
+                        }
+                        else
+                        {
+                            regularGuests.Remove(currentGuests);
+                        }
+
+                        if (currentGuests == "END")
+                        {
+                            break;
+                        }
+                    }
+                    break;
+                }
+                else
+                {
+                    if (IsMatched(inputline))
+                    {
+                        vip.Add(inputline);
+                    }
+                    else
+                    {
+                        regularGuests.Add(inputline);
+                    }
+                }
+            }
+            PrintingGuestLists(vip, regularGuests);
+        }
+
+        static bool IsMatched(string inputline)
+        {
+            var pattern = @"(?<VIP>^\d)";
+            Regex regex = new Regex(pattern);
+            MatchCollection matchedInput = regex.Matches(inputline);
+
+            if (!matchedInput.Any())
+            {
+                return false;
+            }
+            return true;
+        }
+
+        static void PrintingGuestLists(HashSet<string> vip, HashSet<string> regularGuests)
+        {
+            int countOfMissingGuests = vip.Count + regularGuests.Count;
+
+            Console.WriteLine(countOfMissingGuests);
+
+            foreach (var vipGuests in vip)
+            {
+                Console.WriteLine(vipGuests);
+            }
+
+            foreach (var regular in regularGuests)
+            {
+                Console.WriteLine(regular);
+            }
+        }
+    }
+}
