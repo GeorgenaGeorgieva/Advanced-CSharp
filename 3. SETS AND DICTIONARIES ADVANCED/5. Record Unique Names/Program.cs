6568508417aefb716845baf20d3e44e19d351870
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-11 22:45:40 +0200

    CSharp

---
+++ 3. SETS AND DICTIONARIES ADVANCED/5. Record Unique Names/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _5._Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfNames = int.Parse(Console.ReadLine());
+            HashSet<string> uniqueNmaes = new HashSet<string>();
+
+            for (int i = 0; i < countOfNames; i++)
+            {
+                var name = Console.ReadLine();
+                uniqueNmaes.Add(name);
+            }
+
+            foreach (var names in uniqueNmaes)
+            {
+                Console.WriteLine(names);
+            }
+        }
+    }
+}
