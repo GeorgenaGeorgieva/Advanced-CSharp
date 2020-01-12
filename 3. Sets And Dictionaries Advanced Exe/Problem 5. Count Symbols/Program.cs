c483f32140c927c8c77c66c5c892ccf89f4dd669
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-12 23:03:20 +0200

    CSharp

---
+++ 3. Sets And Dictionaries Advanced Exe/Problem 5. Count Symbols/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Problem_5._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var characters = new Dictionary<char, int>();
+            string inputLine = Console.ReadLine();
+
+            for (int i = 0; i < inputLine.Length; i++)
+            {
+                var symbol = inputLine[i];
+
+                if (!characters.ContainsKey(symbol))
+                {
+                    characters.Add(symbol, 0);
+                }
+                characters[symbol]++;
+            }
+
+            characters = characters.OrderBy(x => x.Key).ToDictionary(s => s.Key, y => y.Value);
+
+            foreach (var kvp in characters)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
