cb48bae12ef0d3d12eda8a9510abe83cc5553263
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-12 23:02:33 +0200

    CSharp

---
+++ 3. Sets And Dictionaries Advanced Exe/Problem 4. Even Times/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Problem_4._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfLines = int.Parse(Console.ReadLine());
+            var numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < countOfLines; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(currentNumber))
+                {
+                    numbers.Add(currentNumber, 0);
+                }
+                numbers[currentNumber]++;
+            }
+
+            foreach (var kvp in numbers)
+            {
+                if (kvp.Value % 2 == 0)
+                {
+                    Console.WriteLine(kvp.Key);
+                }
+            }
+        }
+    }
+}
