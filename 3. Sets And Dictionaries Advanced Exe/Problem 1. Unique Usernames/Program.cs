87099ed0bbc01239805982536693e58fea2b0d0f
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-12 22:55:29 +0200

    CSharp

---
+++ 3. Sets And Dictionaries Advanced Exe/Problem 1. Unique Usernames/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_1._Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfNames = int.Parse(Console.ReadLine());
+            var uniqueUsers = new HashSet<string>();
+
+            for (int i = 0; i < countOfNames; i++)
+            {
+                var currentName = Console.ReadLine();
+                uniqueUsers.Add(currentName);
+            }
+
+            foreach (var user in uniqueUsers)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
