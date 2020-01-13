ad4c5a1c2ffbd3fff53fff953930cdf5a6e6f86c
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-13 22:45:37 +0200

    CSharp

---
+++ 4.Streams And Directories Lab/2. Line Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace File_Operations_2._Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("Input.txt"))
+            {
+                using (var writer = new StreamWriter("Output.txt"))
+                {
+                    var lineNumber = 1;
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        writer.WriteLine($"{lineNumber}. {line}");
+                        lineNumber++;
+                    }
+                }
+            }
+        }
+    }
+}
