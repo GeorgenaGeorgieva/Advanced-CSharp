59db4b09bf34142b3ee583338736e500262e1535
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-13 22:48:19 +0200

    CSharp

---
+++ 4.Streams And Directories Exe/Problem 1. Even Lines/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Problem_1._Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader(@"text.txt"))
+            {
+                using (var writer = new StreamWriter(@"Output.txt"))
+                {
+                    var counter = 0;
+                    var symbolsToReplace = new[] { '-', ',', '.', '!', '?' };
+
+                    while (!reader.EndOfStream)
+                    {
+                        var lines = reader.ReadLine();
+
+                        if (counter % 2 == 0)
+                        {
+                            var words = lines.Split(' ');
+
+                            for (int i = 0; i < words.Length; i++)
+                            {
+                                var currentWord = words[i];
+
+                                foreach (var symbol in symbolsToReplace)
+                                {
+                                    currentWord = currentWord.Replace(symbol, '@');
+                                }
+
+                                words[i] = currentWord;
+                            }
+
+                            var result = string.Join(" ", words.Reverse());
+                            writer.WriteLine(result);
+                        }
+
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 4.Streams And Directories Exe/Problem 2. Line Numbers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem_2._Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader(@"text.txt"))
+            {
+                using (var writer = new StreamWriter(@"Output.txt"))
+                {
+                    var counter = 1;
+
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var words = line.Split(' ');
+
+                        var countOfLetters = 0;
+                        var countOfPunctuations = 0;
+
+                        for (int i = 0; i < words.Length; i++)
+                        {
+                            var currentWord = words[i];
+
+                            for (int k = 0; k < currentWord.Length; k++)
+                            {
+                                var currentChar = currentWord[k];
+
+                                if (Char.IsLetter(currentChar))
+                                {
+                                    countOfLetters++;
+                                }
+                                else if (Char.IsPunctuation(currentChar))
+                                {
+                                    countOfPunctuations++;
+                                }
+                            }
+                        }
+
+                        writer.WriteLine($"Line {counter}: {line} ({countOfLetters})({countOfPunctuations})");
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
