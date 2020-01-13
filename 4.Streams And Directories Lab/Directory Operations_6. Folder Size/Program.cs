657358813e7fef1e3be511ee11d0a4b53c31d7a7
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-13 22:47:09 +0200

    CSharp

---
+++ 4.Streams And Directories Lab/Directory Operations_6. Folder Size/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Directory_Operations_6._Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles(@"D:\SoftUni\Advance\04.Streams And Directories\Solutions\Directory Operations_6. Folder Size\TestFolder\TestFolder2\");
+            var totalLength = 0L;
+
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                totalLength += fileInfo.Length;
+            }
+
+            var inMegabytes = totalLength * 0.00000095367432; //bytes to Megabytes conversation
+            Console.WriteLine(inMegabytes);
+        }
+    }
+}
---
+++ 4.Streams And Directories Lab/Directory Operations_6. Folder Size_SecondSolution/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Directory_Operations_6._Folder_Size_SecondSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var length = GetLength(Environment.CurrentDirectory);
+            Console.WriteLine(length);
+        }
+
+        static long GetLength(string directory)
+        {
+            var totalLength = 0L;
+            var files = Directory.GetFiles(directory);
+
+            var subDirectories = Directory.GetDirectories(directory);
+
+            foreach (var subDirectory in subDirectories)
+            {
+                totalLength += GetLength(subDirectory);
+            }
+
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                totalLength += fileInfo.Length;
+            }
+            return totalLength;
+        }
+    }
+}
