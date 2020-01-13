d28ae7faf29d10a913aab4241aabb20bcd1d78a5
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-13 22:45:54 +0200

    CSharp

---
+++ 4.Streams And Directories Lab/3. Word Count/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace File_Operations_3._Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader(@"words.txt"))
+            {
+                using (var readSecondFile = new StreamReader(@"text.txt"))
+                {
+                    using (var writer = new StreamWriter(@"Output.txt"))
+                    {
+                        var words = new Dictionary<string, int>();
+
+                        while (!reader.EndOfStream)
+                        {
+                            var currentLine = reader
+                                .ReadLine()
+                                .Split(' ');
+
+                            for (int i = 0; i < currentLine.Length; i++)
+                            {
+                                var currentWord = currentLine[i];
+
+                                if (!words.ContainsKey(currentWord))
+                                {
+                                    words.Add(currentWord, 0);
+                                }
+                            }
+                        }
+
+                        while (!readSecondFile.EndOfStream)
+                        {
+                            var SecondCurrentLine = readSecondFile
+                                .ReadLine()
+                                .Split(' ', ',', '-', '.', '?', '!');
+
+                            for (int i = 0; i < SecondCurrentLine.Length; i++)
+                            {
+                                var currentWord = SecondCurrentLine[i].ToLower();
+
+                                if (words.ContainsKey(currentWord))
+                                {
+                                    words[currentWord]++;
+                                }
+                            }
+                        }
+
+                        words = words
+                            .OrderByDescending(x => x.Value)
+                            .ToDictionary(a => a.Key, z => z.Value);
+
+                        foreach (var kvp in words)
+                        {
+                            writer.WriteLine($"{kvp.Key} - {kvp.Value}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
