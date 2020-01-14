b3d2048d7ad905698ea06d1b07b42658481fc019
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-14 22:14:09 +0200

    CSharp

---
+++ 5.Functional Programming Lab/2. Sum Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _2._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            Func<string, int> parser = n => int.Parse(n);
+
+            int[] numbers = input
+                .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
