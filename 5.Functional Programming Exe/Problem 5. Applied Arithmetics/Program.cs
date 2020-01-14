96cdd3f4c30212693b2024155142c0674976f050
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-14 22:18:13 +0200

    CSharp

---
+++ 5.Functional Programming Exe/Problem 5. Applied Arithmetics/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Problem_5._Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+               .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            Func<int, int> operationAdd = x => ++x;
+            Func<int, int> operationMultiply = x => x *= 2;
+            Func<int, int> operationSubtract = x => --x;
+            Action<int[]> printing = nums => Console.WriteLine(string.Join(" ", nums));
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+
+                if (command == "end")
+                {
+                    break;
+                }
+                else if (command == "add")
+                {
+                    numbers = numbers.Select(operationAdd).ToArray();
+                }
+                else if (command == "multiply")
+                {
+                    numbers = numbers.Select(operationMultiply).ToArray();
+                }
+                else if (command == "subtract")
+                {
+                    numbers = numbers.Select(operationSubtract).ToArray();
+                }
+                else if (command == "print")
+                {
+                    printing(numbers);
+                }  
+            }
+        }
+    }
+}
---
+++ 5.Functional Programming Exe/Problem 6. Reverse and Exclude/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Problem_6._Reverse_and_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numCollection = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            var divider = int.Parse(Console.ReadLine());
+
+            Func<int, bool> operation = num => num % divider != 0;
+            Action<int[]> printing = nums => Console.Write(string.Join(" ", nums));
+
+            numCollection = numCollection
+                 .Where(operation)
+                 .Reverse()
+                 .ToArray();
+
+            printing(numCollection);
+        }
+    }
+}
