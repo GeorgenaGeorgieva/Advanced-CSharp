cb76568e2aae9edc5836bd8ab67ccafa59a8b5fa
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-14 22:13:39 +0200

    CSharp

---
+++ 5.Functional Programming Lab/1. Sort Even Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _1._Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var selectedNumbers = Console.ReadLine()
+                .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToList();
+
+            Console.Write(string.Join(", ", selectedNumbers));
+        }
+    }
+}
