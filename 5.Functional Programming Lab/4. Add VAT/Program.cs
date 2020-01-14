a8debcf292d70b10183c4130aa88233119cfdb66
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-14 22:14:50 +0200

    CSharp

---
+++ 5.Functional Programming Lab/4. Add VAT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _4._Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(x => $"{(x * 1.2):F2}")
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
