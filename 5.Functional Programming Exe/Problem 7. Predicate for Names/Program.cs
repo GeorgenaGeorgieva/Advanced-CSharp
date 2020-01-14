3f53100bc7885a2fb9b76513e940b1304ba21a98
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-14 22:18:34 +0200

    CSharp

---
+++ 5.Functional Programming Exe/Problem 7. Predicate for Names/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Problem_7._Predicate_for_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lengthOfNames = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Func<string, bool> conditionalLength = x => x.Length <= lengthOfNames;
+            Action<string> printing = x => Console.WriteLine(x);
+
+            names
+                .Where(conditionalLength)
+                .ToList()
+                .ForEach(printing);
+        }
+    }
+}
