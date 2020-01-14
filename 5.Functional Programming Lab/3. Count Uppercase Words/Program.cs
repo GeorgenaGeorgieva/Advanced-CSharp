cd2bfd3bed75b8a806240e80fb532693a2e45221
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-14 22:14:32 +0200

    CSharp

---
+++ 5.Functional Programming Lab/3. Count Uppercase Words/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _3._Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => char.IsUpper(x[0]))
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
