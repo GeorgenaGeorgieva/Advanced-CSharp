4d23a42f268d2bd2fae18894a6e74b6dcd323ba1
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-14 22:15:08 +0200

    CSharp

---
+++ 5.Functional Programming Lab/5. Filter by Age/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _5._Filter_by_Age
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfLines = int.Parse(Console.ReadLine());
+            var people = new List<Person>();
+
+            for (int i = 0; i < countOfLines; i++)
+            {
+                var personInfo = Console.ReadLine()
+                    .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var person = new Person();
+                person.Name = personInfo[0];
+                person.Age = int.Parse(personInfo[1]);
+
+                people.Add(person);
+            }
+
+            var condition = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+
+            Func<Person, bool> filterPredicate;
+
+                if (condition == "older")
+                {
+                    filterPredicate = p => p.Age >= age;
+                }
+                else
+                {
+                    filterPredicate = p => p.Age < age;
+                }
+
+            var formatPrinting = Console.ReadLine();
+
+            Func<Person, string> selectFunc;
+
+            if (formatPrinting == "name age")
+            {
+                selectFunc = p => $"{p.Name} - {p.Age}";
+            }
+            else if (formatPrinting == "name")
+            {
+                selectFunc = p => $"{p.Name}";
+            }
+            else
+            {
+                selectFunc = p => $"{p.Age}";
+            }
+   
+            people
+                .Where(filterPredicate)
+                .Select(selectFunc)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
