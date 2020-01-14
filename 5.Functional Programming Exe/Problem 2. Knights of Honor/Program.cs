7a6f9610f381cbfde388fb36897b0d50d0ca144c
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-14 22:16:35 +0200

    CSharp

---
+++ 5.Functional Programming Exe/Problem 2. Knights of Honor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Problem_2._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine();
+            var title = "Sir";
+
+            Action<string> knighthoodCeremony = x => Console.WriteLine($"{title} {x}");
+
+            names
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(knighthoodCeremony);
+        }
+    }
+}
