29ebe0043ea90013d5d84b830aec6f7684a6b91a
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-14 22:16:12 +0200

    CSharp

---
+++ 5.Functional Programming Exe/Problem 1. Action Point/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Problem_1._Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            Action<string> printingResult = x => Console.WriteLine(x);
+
+            input
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(printingResult);   
+        }
+    }
+}
