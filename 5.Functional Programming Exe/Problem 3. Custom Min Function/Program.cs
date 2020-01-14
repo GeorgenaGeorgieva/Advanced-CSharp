648361b805cb5009dd27042adf4eba1b855645a5
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-14 22:17:05 +0200

    CSharp

---
+++ 5.Functional Programming Exe/Problem 3. Custom Min Function/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Problem_3._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = Console.ReadLine()
+                 .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToList()
+                 .Min(x => x)
+                 .ToString();
+
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ 5.Functional Programming Exe/Problem 4. Find Evens or Odds/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Problem_4._Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var range = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var condition = Console.ReadLine();
+
+            Func<int, bool> numberInRange;
+
+            if (condition == "odd")
+            {
+                numberInRange = x => x % 2 != 0;
+            }
+            else
+            {
+                numberInRange = x => x % 2 == 0;
+            }
+
+            var numbers = new List<int>();
+
+            for (int i = range[0]; i <= range[1]; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Action<int> printingList = x => Console.Write($"{x} ");
+
+            numbers
+                .Where(numberInRange)
+                .ToList()
+                .ForEach(printingList);
+        }
+    }
+}
