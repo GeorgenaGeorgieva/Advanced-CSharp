d92a99bde6f39a408d28ef0c4bc0834e4dc23541
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-15 13:24:48 +0200

    CSharp

---
+++ 5.Functional Programming Exe/Problem 8. Custom Comparator/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Problem_8._Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, int> compareEven = (a, b) =>
+            {
+                if (a % 2 == 0 && b % 2 == 0)
+                {
+                    if (a < b)
+                    {
+                        return -1;
+                    }
+
+                    if (a > b)
+                    {
+                        return 1;
+                    }
+
+                    return 0;
+                }
+
+                if (a % 2 != 0 && b % 2 != 0)
+                {
+                    if (a < b)
+                    {
+                        return -1;
+                    }
+
+                    if (a > b)
+                    {
+                        return 1;
+                    }
+
+                    return 0;
+                }
+
+                if (a % 2 == 0)
+                {
+                    return -1;
+                }
+
+                if (a % 2 != 0)
+                {
+                    return 1;
+                }
+
+                return 0;
+            };
+
+            Array.Sort(numbers, new Comparison<int>(compareEven));
+
+            Action<int[]> printing = nums => Console.Write(string.Join(" ", nums));
+
+            printing(numbers);
+        }
+    }
+}
