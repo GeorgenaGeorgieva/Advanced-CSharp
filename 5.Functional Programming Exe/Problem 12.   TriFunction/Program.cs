d7395c2e8663de6948b61c803b2caa2caa8e0017
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-15 13:26:08 +0200

    CSharp

---
+++ 5.Functional Programming Exe/Problem 12.   TriFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_12.___TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNumber = int.Parse(Console.ReadLine());
+
+            Func<string, char[]> funcToCharArray = n => n.ToCharArray();
+            Func<char, int> castFunc = z => (int)z;
+            Func<string, bool> funcFinal = n => funcToCharArray(n).Select(castFunc).Sum() >= inputNumber;
+
+            var namesLine = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .FirstOrDefault(funcFinal);
+
+            Console.WriteLine(namesLine);
+        }
+    }
+}
