49608e673bde193acf55a2e7d6b3095b823d36be
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-15 13:25:09 +0200

    CSharp

---
+++ 5.Functional Programming Exe/Problem 9. List of Predicates/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Problem_9._List_of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rangeBorder = int.Parse(Console.ReadLine());
+            var dividers = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Distinct()
+                .ToList();
+
+            var numbersToDivide = new List<int>();
+
+            for (int num = 1; num <= rangeBorder; num++)
+            {
+                var isDivisible = true;
+
+                foreach (var divider in dividers)
+                {
+                    Func<int, bool> isNotDivider = x => num % x != 0;
+
+                    if (isNotDivider(divider))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    numbersToDivide.Add(num);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbersToDivide));
+        }
+    }
+}
