82a8e50622a5c3e5c4df8a638db32cf77a76a83e
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-16 13:48:43 +0200

    CSharp

---
+++ 06.Defining Classes Lab/Problem 2. Car Extension/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace Problem_2._Car_Extension
+{
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            var canContinue = this.FuelQuantity - this.FuelConsumption * distance / 100 >= 0;
+
+            if (canContinue)
+            {
+                this.FuelQuantity -= distance / 100 * this.FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine($"Make: {this.Make}");
+            result.AppendLine($"Model: {this.Model}");
+            result.AppendLine($"Year: {this.Year}");
+            result.Append($"Fuel: {this.FuelQuantity:F2}L");
+
+            return result.ToString();
+        }
+    }
+}
---
+++ 06.Defining Classes Lab/Problem 2. Car Extension/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Problem_2._Car_Extension;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+            car.Drive(2000);
+
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
