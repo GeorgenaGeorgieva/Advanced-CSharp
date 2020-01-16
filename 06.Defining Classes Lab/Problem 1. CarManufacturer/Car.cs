d765eacd4d79951fbf22cc77186fa7feac71ccfa
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-16 13:48:26 +0200

    CSharp

---
+++ 06.Defining Classes Lab/Problem 1. CarManufacturer/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace Problem1.Car
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+    }
+}
---
+++ 06.Defining Classes Lab/Problem 1. CarManufacturer/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Problem1.Car;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Make = "WV";
+            car.Model = "MK3";
+            car.Year = 1992;
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
