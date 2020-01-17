83355fe077637773f41481b222c6cd471a2ef808
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-17 20:57:56 +0200

    CSharp

---
+++ 06.Defining Classes Exe/Problem 3. Oldest Family Member/Family.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+
+    public class Family
+    {
+        private List<Person> members;
+
+        public Family()
+        {
+            this.members = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            this.members.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.members.OrderBy(x => x.Age).Last();
+        }
+
+        public List<Person> Person { get; set; }
+    }
+}
---
+++ 06.Defining Classes Exe/Problem 3. Oldest Family Member/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Family family = new Family();
+
+            int numberOfFamilyMembers = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfFamilyMembers; i++)
+            {
+                string[] currentMemberInformation = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string name = currentMemberInformation[0];
+                int age = int.Parse(currentMemberInformation[1]);
+
+                Person member = new Person(name, age);
+                family.AddMember(member);
+            }
+
+            Person oldestMember = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
