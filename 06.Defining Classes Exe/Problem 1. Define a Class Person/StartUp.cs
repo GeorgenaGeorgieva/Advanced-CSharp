ee71dba681d36615274a33f97e5fdd238d606492
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-17 20:57:02 +0200

    CSharp

---
+++ 06.Defining Classes Exe/Problem 1. Define a Class Person/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using DefiningClasses_Person;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person firstPerson = new Person();
+
+            firstPerson.Name = "Pesho";
+            firstPerson.Age = 20;
+
+            Person thirdPerson = new Person
+            {
+                Name = "Stamat"
+            };
+
+            thirdPerson.Age = 43;
+
+
+            Person secondPerson = new Person
+            {
+                Name = "Gosho",
+                Age = 18
+            };
+        }
+    }
+}
