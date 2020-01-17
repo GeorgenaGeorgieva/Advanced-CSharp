3fdae8fb01a55676671f1b781ce65e842e1a14f5
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-17 20:58:59 +0200

    CSharp

---
+++ 06.Defining Classes Exe/Problem 6. Speed Racing/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double travelledDistance;
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+
+            set { this.model = value; }
+        }
+
+        public double FuelAmount
+        {
+            get { return this.fuelAmount; }
+            set { this.fuelAmount = value; }
+        }
+
+        public double FuelConsumptionPerKilometer
+        {
+            get { return this.fuelConsumptionPerKilometer; }
+            set { this.fuelConsumptionPerKilometer = value; }
+        }
+
+        public double TravelledDistance
+        {
+            get { return this.travelledDistance; }
+            set { this.travelledDistance = value; }
+        }
+
+        public bool Drive(int distance)
+        {
+            var fuelNeeded = distance * this.FuelConsumptionPerKilometer;
+
+            if (fuelNeeded <= this.FuelAmount)
+            {
+                this.FuelAmount -= fuelNeeded;
+                this.TravelledDistance += distance;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 06.Defining Classes Exe/Problem 6. Speed Racing/StartUp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var carsCollection = new Dictionary<string, Car>();
+            var count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                var carInformation = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                var model = carInformation[0];
+                var fielAmount = double.Parse(carInformation[1]);
+                var fuelConsumptionForKm = double.Parse(carInformation[2]);
+
+                if (!carsCollection.ContainsKey(model))
+                {
+                    Car currentCar = new Car(model, fielAmount, fuelConsumptionForKm);
+                    carsCollection.Add(model, currentCar);
+                }
+            }
+
+            while (true)
+            {
+                var commands = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                if (commands[0] == "End")
+                {
+                    break;
+                }
+                else if (commands[0] == "Drive")
+                {
+                    var carModel = commands[1];
+                    var distanceTraveled = int.Parse(commands[2]);
+
+                    Car car = carsCollection[carModel];
+
+                    bool IsEnoughFuelToDrive = car.Drive(distanceTraveled);
+
+                    if (!IsEnoughFuelToDrive)
+                    {
+                        Console.WriteLine("Insufficient fuel for the drive");
+                    }                    
+                }
+            }
+
+            foreach (var kvp in carsCollection)
+            {
+                Console.WriteLine($"{kvp.Value.Model} {kvp.Value.FuelAmount:F2} {kvp.Value.TravelledDistance}");
+            }
+        }
+    }
+}
