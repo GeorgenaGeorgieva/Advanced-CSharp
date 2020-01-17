74fb73983e6661c110db5836770cf5ab629db4bd
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-17 20:57:36 +0200

    CSharp

---
+++ 06.Defining Classes Exe/Problem 2. Creating Constructors/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DefiningClasses;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person firstPerson = new Person();
+            Person secondPerson = new Person(56);
+            Person thirdPerson = new Person("Pesho", 28);
+        }
+    }
+}
