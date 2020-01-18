2da7c2e6737748abe673d94b8e3bf67746923e54
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-18 23:11:24 +0200

    CSharp

---
+++ 07.LinkedList/Linked List_Array/CoolStack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List_Array
+{
+    public class CoolStack
+    {
+        private object[] values;
+        private int pushCount;
+
+        public CoolStack()
+            : this(16)
+        {
+        }
+
+        public CoolStack(int initialCapacity)
+        {
+            this.values = new object[initialCapacity];
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.pushCount;
+            }
+        }
+
+        public void Push(object value)
+        {
+            if (this.pushCount == this.values.Length)
+            {
+                var nextValues = new object[this.values.Length * 2];
+
+                for (int i = 0; i < this.values.Length; i++)
+                {
+                    nextValues[i] = this.values[i];
+                }
+
+                this.values = nextValues;
+            }
+
+            this.values[this.pushCount] = value;
+            this.pushCount++;
+
+        }
+
+        public object Pop()
+        {
+            if (this.pushCount == 0)
+            {
+                throw new InvalidOperationException("Cool stack is emty!");
+            }
+
+            var lastIndex = this.pushCount - 1;
+            var last = this.values[lastIndex];
+            this.pushCount--;
+            return last;
+
+        }
+
+        public void ForEach(Action<object> action)
+        {
+            for (int i = 0; i < this.pushCount; i++)
+            {
+                action(this.values[i]);
+            }
+        }
+    }
+}
---
+++ 07.LinkedList/Linked List_Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Linked_List_Array
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var stack = new CoolStack();
+
+            stack.Push(2);
+            stack.Push(5);
+            stack.Push(10);
+
+            Console.WriteLine(stack.Count == 3);
+
+            var value = (int)stack.Pop();
+
+            Console.WriteLine(value == 10);
+
+            Console.WriteLine(stack.Count == 2);
+
+            stack.ForEach(obj => Console.WriteLine(obj));
+        }
+    }
+}
