4d0911c1228a8f0ba58ec1943aa93e9bc4477649
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-18 23:13:30 +0200

    CSharp

---
+++ 08.Generics Lab/3. Generic Scale/StartUp .cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string leftElement = "Pesho";
+            string rightElement = "Gosho";
+
+            EqualityScale<string> scaleString = new EqualityScale<string>(leftElement, rightElement);
+
+            Console.WriteLine(scaleString.AreEqual());
+
+            int leftInteger = 5;
+            int rightInteger = 5;
+
+            EqualityScale<int> scaleInteger = new EqualityScale<int>(leftInteger, rightInteger);
+
+            Console.WriteLine(scaleInteger.AreEqual());
+        }
+    }
+}
