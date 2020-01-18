2551b2256e1a42d73bec8bac592aea9863e59531
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-18 23:12:57 +0200

    CSharp

---
+++ 08.Generics Lab/1. Box of T/Box.cs
@@ -0,0 +1,32 @@
+﻿namespace BoxOfT
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Box<T>
+    {
+        private List<T> elements;
+
+        public Box()
+        {
+            this.elements = new List<T>();
+        }
+
+        public int Count
+        {
+            get { return this.elements.Count; }
+        }
+
+        public void Add(T element)
+        {
+            this.elements.Add(element);
+        }
+
+        public T Remove()
+        {
+            var lastElement = this.elements.Last();
+            this.elements.RemoveAt(this.elements.Count - 1);
+            return lastElement;
+        }
+    }
+}
