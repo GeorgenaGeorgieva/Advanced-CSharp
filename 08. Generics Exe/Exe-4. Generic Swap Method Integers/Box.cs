9f494455e2f33bfc6e6e43d8a122a4493efcdbc4
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-18 23:15:18 +0200

    CSharp

---
+++ 08. Generics Exe/Exe-4. Generic Swap Method Integers/Box.cs
@@ -0,0 +1,31 @@
+﻿namespace SwapMethodIntegers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box<T>
+    {
+        private List<T> elements;
+
+        public Box()
+        {
+            this.Elements = new List<T>();
+        }
+
+        public List<T> Elements
+        {
+            get { return this.elements; }
+            set { this.elements = value; }
+        }
+
+        public List<T> SwapElements(int firstIndex, int secondIndex)
+        {
+            var firstElement = this.elements[firstIndex];
+            this.elements[firstIndex] = this.elements[secondIndex];
+            this.elements[secondIndex] = firstElement;
+
+            return this.elements;
+        }
+    }
+}
---
+++ 08. Generics Exe/Exe-4. Generic Swap Method Integers/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace SwapMethodIntegers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int countOfLines = int.Parse(Console.ReadLine());
+            Box<int> boxWithNumbers = new Box<int>();
+
+            for (int i = 0; i < countOfLines; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                boxWithNumbers.Elements.Add(currentNumber);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstIndex = indexes[0];
+            int secondIndex = indexes[1];
+
+            List<int> newSwapedElementsBox = boxWithNumbers.SwapElements(firstIndex, secondIndex);
+
+            foreach (var num in newSwapedElementsBox)
+            {
+                Console.WriteLine($"{num.GetType()}: {num}");
+            }
+        }
+    }
+}
