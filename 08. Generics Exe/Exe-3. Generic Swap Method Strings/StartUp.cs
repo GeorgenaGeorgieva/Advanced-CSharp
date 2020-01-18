f1344dd9f3f74a4f2dce22c40eea6c7721c302fa
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-18 23:15:01 +0200

    CSharp

---
+++ 08. Generics Exe/Exe-3. Generic Swap Method Strings/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace SwapMethodStrings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SartUp
+    {
+        public static List<T> SwapStrings<T>(int firstIndex, int secondIndex, List<T> stringsList)
+        {
+            var firstElement = stringsList.ElementAt(firstIndex);
+            var secondElement = stringsList.ElementAt(secondIndex);
+            stringsList[secondIndex] = firstElement;
+            stringsList[firstIndex] = secondElement;
+
+            return stringsList;
+        }
+
+        public static void Main(string[] args)
+        {
+            int countOfLines = int.Parse(Console.ReadLine());
+            List<string> stringsList = new List<string>();
+
+            for (int i = 0; i < countOfLines; i++)
+            {
+                string inputLine = Console.ReadLine();
+                stringsList.Add(inputLine);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstIndex = indexes[0];
+            int secondIndex = indexes[1];
+
+            SwapStrings<string>(firstIndex, secondIndex, stringsList);
+
+            foreach (var line in stringsList)
+            {
+                Console.WriteLine($"{line.GetType()}: {line}");
+            }
+        }
+    }
+}
