121526241dff6881ceea1ced13b486c74901e0d8
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-21 19:58:30 +0200

    CSharp

---
+++ 08. Generics Exe/Exe-5. Generic Count Method Strings/Box.cs
@@ -0,0 +1,22 @@
+﻿namespace CountMethodStrings
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Box<T> 
+    where T : IComparable<T>
+    {
+        private List<T> elements;
+
+        public Box()
+        {
+            this.Elements = new List<T>();
+        }
+        
+        public List<T> Elements
+        {
+            get { return this.elements; }
+            set { this.elements = value; }
+        }
+    }
+}
---
+++ 08. Generics Exe/Exe-5. Generic Count Method Strings/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace CountMethodStrings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int countOfInputLines = int.Parse(Console.ReadLine());
+            Box<string> boxWithData = new Box<string>();
+
+            for (int i = 0; i < countOfInputLines; i++)
+            {
+                var inputLine = Console.ReadLine();
+                boxWithData.Elements.Add(inputLine);
+            }
+
+            string comparator = Console.ReadLine();
+
+            var result = GetCountOfGreaterElements<string>(boxWithData.Elements, comparator);
+
+            Console.WriteLine(result);
+        }
+
+        public static int GetCountOfGreaterElements<T>(List<T> listWithData, T element)
+            where T: IComparable
+        {
+            int counter = 0;
+
+            foreach (var item in listWithData)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    counter++;
+                }
+            }
+         
+            return counter;
+        }
+    }
+}
