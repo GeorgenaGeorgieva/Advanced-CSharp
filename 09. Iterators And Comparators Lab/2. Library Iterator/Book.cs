3575d3fd93920138140217a519e820a38d850659
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-21 20:05:52 +0200

    CSharp

---
+++ 09. Iterators And Comparators Lab/2. Library Iterator/Book.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        private string title;
+        private int year;
+        private List<string> authors;
+
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.authors = new List<string>(authors);
+        }
+
+        public string Title 
+        {
+            get {return this.title; }
+            private set {this.title = value; }
+        }
+
+        public int Year
+        {
+            get { return this.year; }
+            private set { this.year = value; }
+        }
+
+        public IReadOnlyList<string> Authors
+        {
+            get { return this.authors.AsReadOnly(); }
+        }
+    }
+}
