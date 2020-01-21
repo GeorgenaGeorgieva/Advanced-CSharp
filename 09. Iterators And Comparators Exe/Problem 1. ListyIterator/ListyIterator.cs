f4bc7e15027e398cd22012664e4fe4e186d14a6b
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-21 20:08:05 +0200

    CSharp

---
+++ 09. Iterators And Comparators Exe/Problem 1. ListyIterator/ListyIterator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem_1._ListyIterator
+{
+    public class ListyIterator<T>
+    {
+    }
+}
---
+++ 09. Iterators And Comparators Exe/Problem 1. ListyIterator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem_1._ListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
