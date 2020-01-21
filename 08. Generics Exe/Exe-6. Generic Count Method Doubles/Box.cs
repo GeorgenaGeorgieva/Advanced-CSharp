814c80f47d7edc23c165abd6aaf7e614a884a618
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-01-21 19:58:48 +0200

    CSharp

---
+++ 08. Generics Exe/Exe-6. Generic Count Method Doubles/Box.cs
@@ -0,0 +1,26 @@
+﻿namespace CountMethodDoubles
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Box<T> where T: IComparable
+    {
+        private List<T> elements;
+
+        public Box()
+        {
+            this.Elements = new List<T>();
+        }
+
+        public List<T> Elements
+        {
+            get { return this.elements; }
+            private set { this.elements = value; }
+        }
+
+        public void Add(T element)
+        {
+            this.elements.Add(element);
+        }
+    }
+}
---
+++ 08. Generics Exe/Exe-6. Generic Count Method Doubles/Startup.cs
@@ -0,0 +1,41 @@
+﻿namespace CountMethodDoubles
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            int countOfInputs = int.Parse(Console.ReadLine());
+            Box<double> box = new Box<double>();
+
+            for (int i = 0; i < countOfInputs; i++)
+            {
+                double currentNumber = double.Parse(Console.ReadLine());
+                box.Add(currentNumber);
+            }
+
+            double comparator = double.Parse(Console.ReadLine());
+            int result = GetCountOfGreaterElements<double>(box.Elements, comparator);
+
+            Console.WriteLine(result);
+        }
+
+        public static int GetCountOfGreaterElements<T>(List<T> listWithData, T comparator)
+            where T: IComparable
+        {
+            int counter = 0;
+
+            foreach (var item in listWithData)
+            {
+                if (item.CompareTo(comparator) > 0)
+                {
+                    counter++;
+                }
+            }
+  
+            return counter;
+        }
+    }
+}
