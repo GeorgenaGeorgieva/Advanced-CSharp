d73e2096788792522ca25059d3258227c33bc5f5
Georgena Georgieva <georgena1991@gmail.com>
2020-01-27 14:33:31 +0200

    Update Program.cs

--- 01. Stacks and Queues Exe/6. Auto Repair and Service/Program.cs
+++ 01. Stacks and Queues Exe/6. Auto Repair and Service/Program.cs
@@ -1,21 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace Repair_and_Service
+namespace RepairAndService
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    
     class Program
     {
         static void Main(string[] args)
         {
-            var cars = Console.ReadLine().Split(" ");
+            string[] cars = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            
             Queue<string> queueCars = new Queue<string>(cars);
             Stack<string> servedCars = new Stack<string>();
             
             while (true)
             {
-                var comands = Console.ReadLine().Split(" ", '-');
-                var currentComand = comands[0];
+                string[] comands = Console.ReadLine()
+                    .Split(new[] { ' ', '-' }, StringSplitOptions.RemoveEmptyEntries);
+                
+                string currentComand = comands[0];
 
                 if (currentComand == "End")
                 {
@@ -25,6 +29,7 @@ namespace Repair_and_Service
                 {
                     string served = queueCars.Peek();
                     servedCars.Push(served);
+                    
                     Console.WriteLine($"Vehicle {served} got served.");
                     queueCars.Dequeue();
                 }
@@ -34,7 +39,9 @@ namespace Repair_and_Service
                 }
                 else
                 {
-                    var carInfo = currentComand.Split('-');
+                    string[] carInfo = currentComand
+                        .Split(new[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+                    
                     string comand = carInfo[0];
                     string modelName = carInfo[1];
 
@@ -42,16 +49,19 @@ namespace Repair_and_Service
                     {
                         Console.WriteLine($"Still waiting for service.");
                     }
+                    
                     if (servedCars.Contains(modelName) && comand == "CarInfo")
                     {
                         Console.WriteLine($"Served.");
                     }
                 }
             }
+            
             if (queueCars.Any())
             {
                 Console.WriteLine($"Vehicles for service: {string.Join(",", queueCars)}");
             }
+            
             if (servedCars.Any())
             {
                 Console.WriteLine($"Served vehicles: {string.Join(",", servedCars)}");
