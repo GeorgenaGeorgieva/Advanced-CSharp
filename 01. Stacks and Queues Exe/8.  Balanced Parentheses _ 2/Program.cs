d1994b819fc8f49db2f1d978efa76187ee2478b3
Georgena Georgieva <georgena1991@gmail.com>
2020-01-27 14:40:14 +0200

    Update Program.cs

--- 01. Stacks and Queues Exe/8.  Balanced Parentheses _ 2/Program.cs
+++ 01. Stacks and Queues Exe/8.  Balanced Parentheses _ 2/Program.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-
-namespace Problem_8.__Balanced_Parentheses___2
+namespace BalancedParentheses2
 {
+     using System;
+     using System.Linq;
+     using System.Collections.Generic;
+    
     class Program
     {
         static void Main(string[] args)
         {
-            var input = Console.ReadLine();
+            string input = Console.ReadLine();
             Stack<char> stack = new Stack<char>();
 
             if (input.Length % 2 != 0)
@@ -22,8 +22,8 @@ namespace Problem_8.__Balanced_Parentheses___2
                 if (bracket == '{' || bracket == '(' || bracket == '[')
                 {
                     stack.Push(bracket);
-                    
                 }
+                
                 if ((bracket == '}' || bracket == ')' || bracket == ']') && stack.Count > 0)
                 {
                     bool isValidFirstCase = stack.Peek() == '(' && bracket == ')';
@@ -45,8 +45,10 @@ namespace Problem_8.__Balanced_Parentheses___2
                     Console.WriteLine("NO");
                     return;
                 }
+                
                 break;
             }
+            
             Console.WriteLine("YES");
         }
     }
