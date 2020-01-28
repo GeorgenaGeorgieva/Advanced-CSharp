b5543267e7e077496ba75a14518ae78c9d5c4fdf
Georgena Georgieva <georgena1991@gmail.com>
2020-01-28 20:35:17 +0200

    Update Program.cs

--- 01. Stacks and Queues Lab/2. Stack Sum/Program.cs
+++ 01. Stacks and Queues Lab/2. Stack Sum/Program.cs
@@ -1,22 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace _2._Stack_Sum
+namespace StackSum
 {
+     using System;
+     using System.Collections.Generic;
+     using System.Linq;
+    
     class Program
     {
         static void Main(string[] args)
         {
-            var numbers = Console.ReadLine().Split(" ");
             Stack<int> stack = new Stack<int>();
+            
+            string[] numbers = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            
             foreach (var number in numbers)
             {
                 stack.Push(int.Parse(number));
             }
+            
             while (true)
             {
-                string[] comands = Console.ReadLine().ToLower().Split(" ").ToArray();
+                string[] comands = Console.ReadLine()
+                    .ToLower()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                
                 string comand = comands[0];
 
                 if (comand == "end")
@@ -27,12 +36,14 @@ namespace _2._Stack_Sum
                 {
                     int firstNumber = int.Parse(comands[1]);
                     int secondNumber = int.Parse(comands[2]);
+                    
                     stack.Push(firstNumber);
                     stack.Push(secondNumber);
                 }
                 else if (comand == "remove")
                 {
                     int countOfNumbers = int.Parse(comands[1]);
+                    
                     if (stack.Count >= countOfNumbers)
                     {
                         for (int i = 1; i <= countOfNumbers; i++)
@@ -42,11 +53,14 @@ namespace _2._Stack_Sum
                     }
                 }
             }
+            
             int sumStackNumbers = 0;
+            
             foreach (var number in stack)
             {
                 sumStackNumbers += number;
             }
+            
             Console.WriteLine($"Sum: {sumStackNumbers}");
         }
     }
