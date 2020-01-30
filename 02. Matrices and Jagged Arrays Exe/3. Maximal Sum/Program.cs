9b1d0cc806f5d5f62259819a901833b55806d20d
Georgena Georgieva <georgena1991@gmail.com>
2020-01-30 12:17:56 +0200

    Update Program.cs

--- 02. Matrices and Jagged Arrays Exe/3. Maximal Sum/Program.cs
+++ 02. Matrices and Jagged Arrays Exe/3. Maximal Sum/Program.cs
@@ -1,26 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace _3._Maximal_Sum
+namespace MaximalSum
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    
     class Program
     {
         static void Main(string[] args)
         {
             int[] matrixSize = Console.ReadLine()
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToArray();
 
             int rows = matrixSize[0];
             int cols = matrixSize[1];
+            
             int[,] matrix = new int[rows, cols];
 
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
                 int[] colsInfo = Console.ReadLine()
-                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
                     .Select(int.Parse)
                     .ToArray();
 
@@ -47,7 +48,8 @@ namespace _3._Maximal_Sum
                     {
                         maxSum = currentSum;
 
-                        tripleMatrix = new int[,] {
+                        tripleMatrix = new int[,] 
+                        {
                             {matrix[row, col], matrix[row, col + 1], matrix[row, col + 2]},
                             {matrix[row + 1, col], matrix[row + 1, col + 1], matrix[row + 1, col + 2]},
                             {matrix[row + 2, col], matrix[row + 2, col + 1], matrix[row + 2, col + 2]}
@@ -55,6 +57,7 @@ namespace _3._Maximal_Sum
                     }
                 }
             }
+            
             Console.WriteLine($"Sum = {maxSum}");
 
             for (int row = 0; row < tripleMatrix.GetLength(0); row++)
@@ -63,6 +66,7 @@ namespace _3._Maximal_Sum
                 {
                     Console.Write(tripleMatrix[row,col] + " ");
                 }
+                
                 Console.WriteLine();
             }
         }
