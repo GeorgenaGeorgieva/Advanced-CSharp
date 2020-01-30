52eb7d278a4df0d977e7fe91f029338a6dee25f0
Georgena Georgieva <georgena1991@gmail.com>
2020-01-30 12:13:17 +0200

    Update Program.cs

--- 02. Matrices and Jagged Arrays Exe/2. 2x2 Squares in Matrix/Program.cs
+++ 02. Matrices and Jagged Arrays Exe/2. 2x2 Squares in Matrix/Program.cs
@@ -1,25 +1,26 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-
-namespace _2._2x2_Squares_in_Matrix
+namespace 2x2SquaresInMatrix
 {
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    
     class Program
     {
         static void Main(string[] args)
         {
             int[] matrixSize = Console.ReadLine()
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToArray();
 
             int rows = matrixSize[0];
             int cols = matrixSize[1];
+            
             char[,] matrix = new char[rows, cols];
 
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
-                var colsInformation = Console.ReadLine()
+                char[] colsInformation = Console.ReadLine()
                     .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
                     .Select(char.Parse)
                     .ToArray();
@@ -29,6 +30,7 @@ namespace _2._2x2_Squares_in_Matrix
                     matrix[row, col] = colsInformation[col];
                 }
             }
+            
             int countOfEqualSquares = 0;
             
             for (int row = 0; row < (matrix.GetLength(0) - 1); row++)
@@ -43,6 +45,7 @@ namespace _2._2x2_Squares_in_Matrix
                     }
                 }
             }
+            
             Console.WriteLine(countOfEqualSquares);
         }
     }
