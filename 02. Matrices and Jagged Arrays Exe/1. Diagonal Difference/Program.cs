b912371ca8dda62ce71c8ca6a3f550b8812c3ec5
Georgena Georgieva <georgena1991@gmail.com>
2020-01-30 12:08:14 +0200

    Update Program.cs

--- 02. Matrices and Jagged Arrays Exe/1. Diagonal Difference/Program.cs
+++ 02. Matrices and Jagged Arrays Exe/1. Diagonal Difference/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-namespace _1._Diagonal_Difference
+namespace DiagonalDifference
 {
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    
     class Program
     {
         static void Main(string[] args)
@@ -10,19 +11,22 @@ namespace _1._Diagonal_Difference
             int sizeMatrix = int.Parse(Console.ReadLine());
             int rows = sizeMatrix;
             int cols = sizeMatrix;
+            
             int[,] matrix = new int[rows, cols];
 
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
                 int[] colsInforamtion = Console.ReadLine()
-                    .Split(" ")
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
                     .Select(int.Parse)
                     .ToArray();
+                
                 for (int col = 0; col < matrix.GetLength(1); col++)
                 {
                     matrix[row, col] = colsInforamtion[col];
                 }
             }
+            
             int sumPrimaryDiagonal = 0;
             int currentRow = 0;
             int currentCol = 0;
@@ -54,6 +58,7 @@ namespace _1._Diagonal_Difference
                 currentRow++;
                 currentCol--;
             }
+            
             int absDifference = Math.Abs(sumPrimaryDiagonal - sumSecondaryDiagonal);
             Console.WriteLine(absDifference);
         }
