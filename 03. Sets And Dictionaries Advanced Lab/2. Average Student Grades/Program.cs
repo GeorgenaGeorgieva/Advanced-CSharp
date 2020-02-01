4e7f52adbd5d1fcad05750c6c317c8c5520d02d2
Georgena Georgieva <georgena1991@gmail.com>
2020-02-01 14:57:30 +0200

    Update Program.cs

--- 03. Sets And Dictionaries Advanced Lab/2. Average Student Grades/Program.cs
+++ 03. Sets And Dictionaries Advanced Lab/2. Average Student Grades/Program.cs
@@ -1,20 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace _2._Average_Student_Grades
+namespace AverageStudentGrades
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    
     class Program
     {
         static void Main(string[] args)
         {
-            int countOfStudents = int.Parse(Console.ReadLine());
-
             Dictionary<string, List<double>> studentsGrade = new Dictionary<string, List<double>>();
+            
+            int countOfStudents = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < countOfStudents; i++)
             {
-                string[] studentsInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string[] studentsInfo = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                
                 string name = studentsInfo[0];
                 double grade = double.Parse(studentsInfo[1]);
 
@@ -22,6 +24,7 @@ namespace _2._Average_Student_Grades
                 {
                     studentsGrade[name] = new List<double>();
                 }
+                
                 studentsGrade[name].Add(grade);
             }
 
@@ -33,6 +36,7 @@ namespace _2._Average_Student_Grades
                 {
                     Console.Write($"{grade:F2} ");
                 }
+                
                 Console.WriteLine($"(avg: {kvp.Value.Average():F2})");
             }
         }
