d6ff62ab6033302445bbadd890c51759102645a9
Georgena Georgieva <georgena1991@gmail.com>
2020-02-03 12:57:00 +0200

    Update Program.cs

--- 04.Streams And Directories Lab/2. Line Numbers/Program.cs
+++ 04.Streams And Directories Lab/2. Line Numbers/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.IO;
-
-namespace File_Operations_2._Line_Numbers
+namespace LineNumbers
 {
+    using System;
+    using System.IO;
+    
     class Program
     {
         static void Main(string[] args)
@@ -12,6 +12,7 @@ namespace File_Operations_2._Line_Numbers
                 using (var writer = new StreamWriter("Output.txt"))
                 {
                     var lineNumber = 1;
+                    
                     while (!reader.EndOfStream)
                     {
                         var line = reader.ReadLine();
