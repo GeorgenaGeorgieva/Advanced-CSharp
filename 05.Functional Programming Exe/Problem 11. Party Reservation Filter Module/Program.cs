5de7e881041d37bbf3aa123546ca7db97ff593e7
Georgena Georgieva <georgena1991@gmail.com>
2020-02-04 17:07:00 +0200

    Update Program.cs

--- 05.Functional Programming Exe/Problem 11. Party Reservation Filter Module/Program.cs
+++ 05.Functional Programming Exe/Problem 11. Party Reservation Filter Module/Program.cs
@@ -1,24 +1,26 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-
-namespace Problem_11._Party_Reservation_Filter_Module
+namespace PartyReservationFilterModule
 {
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    
     class Program
     {
         static void Main(string[] args)
         {
+            List<string> filters = new List<string>();
+            
             List<string> partyGuests = Console.ReadLine()
                  .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
                  .ToList();
-
-            List<string> filters = new List<string>();
-
+            
             string command = Console.ReadLine();
 
             while (command != "Print")
             {
-                string[] commandInfo = command.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] commandInfo = command
+                    .Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                
                 string currentCommand = commandInfo[0];
                 string criterion = commandInfo[1];
                 string parameter = commandInfo[2];
@@ -37,7 +39,9 @@ namespace Problem_11._Party_Reservation_Filter_Module
 
             foreach (var filter in filters)
             {
-                string[] specifications = filter.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] specifications = filter
+                    .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                
                 string currentCriterion = specifications[0];
                 string currentParameter = specifications[1];
 
