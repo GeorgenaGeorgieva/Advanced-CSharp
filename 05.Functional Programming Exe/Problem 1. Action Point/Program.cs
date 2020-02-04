387f7d2180a9cf2f83ee1e45371d46b1a2ae75ab
Georgena Georgieva <georgena1991@gmail.com>
2020-02-04 17:01:28 +0200

    Update Program.cs

--- 05.Functional Programming Exe/Problem 1. Action Point/Program.cs
+++ 05.Functional Programming Exe/Problem 1. Action Point/Program.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-
-namespace Problem_1._Action_Point
+namespace ActionPoint
 {
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    
     class Program
     {
         static void Main(string[] args)
         {
-            var input = Console.ReadLine();
-
+            string input = Console.ReadLine();
             Action<string> printingResult = x => Console.WriteLine(x);
 
             input
