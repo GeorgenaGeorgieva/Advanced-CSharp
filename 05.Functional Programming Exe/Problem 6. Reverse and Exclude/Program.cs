e7c07fa00d141779b9b8eac1932d8571fef42fea
Georgena Georgieva <georgena1991@gmail.com>
2020-02-07 22:47:06 +0200

    Update Program.cs

--- 05.Functional Programming Exe/Problem 6. Reverse and Exclude/Program.cs
+++ 05.Functional Programming Exe/Problem 6. Reverse and Exclude/Program.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-
-namespace Problem_6._Reverse_and_Exclude
+namespace ReverseAndExclude
 {
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    
     class Program
     {
         static void Main(string[] args)
         {
-            var numCollection = Console.ReadLine()
+            int[] numCollection = Console.ReadLine()
                 .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToArray();
-            var divider = int.Parse(Console.ReadLine());
+            
+            int divider = int.Parse(Console.ReadLine());
 
             Func<int, bool> operation = num => num % divider != 0;
             Action<int[]> printing = nums => Console.Write(string.Join(" ", nums));
