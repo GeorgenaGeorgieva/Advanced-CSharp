c0dd4a43850e78c2f1dcd82eb3e773b82006fca7
Georgena Georgieva <georgena1991@gmail.com>
2020-02-07 22:49:34 +0200

    Update Program.cs

--- 05.Functional Programming Exe/Problem 7. Predicate for Names/Program.cs
+++ 05.Functional Programming Exe/Problem 7. Predicate for Names/Program.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-
-namespace Problem_7._Predicate_for_Names
+namespace PredicateForNames
 {
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    
     class Program
     {
         static void Main(string[] args)
         {
-            var lengthOfNames = int.Parse(Console.ReadLine());
-            var names = Console.ReadLine()
+            int lengthOfNames = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine()
                 .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
                 .ToArray();
-
+            
             Func<string, bool> conditionalLength = x => x.Length <= lengthOfNames;
             Action<string> printing = x => Console.WriteLine(x);
 
