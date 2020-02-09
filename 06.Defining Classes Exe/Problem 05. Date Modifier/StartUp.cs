3c9dc029599c8918876be46806d16e1d7ee98caa
Georgena Georgieva <georgena1991@gmail.com>
2020-02-09 21:46:13 +0200

    Update StartUp.cs

--- 06.Defining Classes Exe/Problem 05. Date Modifier/StartUp.cs
+++ 06.Defining Classes Exe/Problem 05. Date Modifier/StartUp.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Linq;
-using System.Globalization;
-using System.Collections.Generic;
-
 namespace DefiningClasses
 {
+    using System;
+    using System.Linq;
+    using System.Globalization;
+    using System.Collections.Generic;
 
-public class StartUp
-{
-    public static void Main(string[] args)
+    public class StartUp
     {
+      public static void Main(string[] args)
+      {
         var firstDate = Console.ReadLine();
         var secondDate = Console.ReadLine();
 
         DateModifier dateModifier = new DateModifier();
 
         Console.WriteLine(dateModifier.ReturnDateDifference(firstDate, secondDate));
-    }
-}
-
-
-
+      }
+   }
 }
