5fb055c8cd486182cf5bfa9c362480df372fd9a9
Georgena Georgieva <georgena1991@gmail.com>
2020-02-12 12:07:47 +0200

    Update StartUp.cs

--- 06.Defining Classes Exe/Problem 07. Raw Data/StartUp.cs
+++ 06.Defining Classes Exe/Problem 07. Raw Data/StartUp.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
 namespace DefiningClasses
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    
     public class StartUp
     {
         public static void Main(string[] args)
