4e7e99440bddaf5c81e21ca3410394cf021b1df2
Georgena Georgieva <georgena1991@gmail.com>
2020-02-12 12:06:41 +0200

    Update Engine.cs

--- 06.Defining Classes Exe/Problem 07. Raw Data/Engine.cs
+++ 06.Defining Classes Exe/Problem 07. Raw Data/Engine.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace DefiningClasses
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    
     public class Engine
     {
         private int speed;
