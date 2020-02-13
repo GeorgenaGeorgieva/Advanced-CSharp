9bb767e3003a330e3ded9fe1e8951136e1ae839a
Georgena Georgieva <georgena1991@gmail.com>
2020-02-13 21:54:59 +0200

    Update StartUp.cs

--- 06.Defining Classes Lab/Problem 5. Special Cars/StartUp.cs
+++ 06.Defining Classes Lab/Problem 5. Special Cars/StartUp.cs
@@ -1,27 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using CarManufacturer;
-
 namespace CarManufacturer
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using CarManufacturer;
+    
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            Dictionary<int, List<double>> carTires = new Dictionary<int, List<double>>();  // years, pressure
-            Dictionary<int, List<double>> carEngines = new Dictionary<int, List<double>>(); // hprsePower, cubicCapacity
-
+            var carTires = new Dictionary<int, List<double>>();  // years, pressure
+            var carEngines = new Dictionary<int, List<double>>(); // hprsePower, cubicCapacity
 
             string inputLine = Console.ReadLine();
             string breakCommand = "No more tires";
-            
 
             CollectingCarParts(inputLine, breakCommand, carTires);
 
             inputLine = Console.ReadLine();
             breakCommand = "Engines done";
-
+            
             //Tire[] tires = new Tire[2];
 
             CollectingCarParts(inputLine, breakCommand, carEngines);
@@ -30,7 +28,10 @@ namespace CarManufacturer
 
             while (inputLine != "Show special")
             {
-                string[] specialCarsCriterion = inputLine.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var specialCarsCriterion = inputLine
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                
                 var currentMake = specialCarsCriterion[0];
                 var currentModel = specialCarsCriterion[1];
                 var currentYear = int.Parse(specialCarsCriterion[2]);
@@ -39,24 +40,26 @@ namespace CarManufacturer
                 var currentEngine = carEngines[int.Parse(specialCarsCriterion[5])];
                 var currentTires = carTires[int.Parse(specialCarsCriterion[6])];
 
-                Car car = new Car(currentMake, currentModel, currentYear, currentFuelQuantity, currentFuelConsumption,)
-              //  { make}
+                Car car = new Car(currentMake, currentModel, currentYear, currentFuelQuantity, currentFuelConsumption)
                 { model}
                 { year}
                 { fuelQuantity}
                 { fuelConsumption}
                 { engineIndex}
                 { tiresIndex}
+                
                 inputLine = Console.ReadLine();
             }
-
         }
 
         private static Dictionary<int, List<double>> CollectingCarParts(string inputLine, string breakcommand, Dictionary<int, List<double>> autoParts)
         {
             while (inputLine != breakcommand)
             {
-                string[] command = inputLine.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string[] command = inputLine
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                
                 int index = 0;
 
                 for (int i = 0; i < command.Length; i += 2)
