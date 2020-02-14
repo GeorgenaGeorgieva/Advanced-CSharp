97d2f72f19e3ede514155651c8cb8ce8867fc83e
Georgena Georgieva <georgena1991@gmail.com>
2020-02-14 22:34:55 +0200

    Update Program.cs

--- 07.LinkedList/Linked List_Workshop/Program.cs
+++ 07.LinkedList/Linked List_Workshop/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Linked_List_Workshop
+namespace LinkedListWorkshop
 {
+    using System;
+    using System.Collections.Generic;
+    
     public class Program
     {
         public static void Main(string[] args)
